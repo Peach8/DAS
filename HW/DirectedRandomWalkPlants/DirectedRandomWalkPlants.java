96e49712abab7b82d30961c6cb8388be27a4da4f
Brandon Peterson <Brandon Peterson>
2016-09-25 00:30:47 -0400

    Draws single stem

--- HW/DirectedRandomWalkPlants/DirectedRandomWalkPlants.java
+++ HW/DirectedRandomWalkPlants/DirectedRandomWalkPlants.java
@@ -5,9 +5,11 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
+import java.awt.geom.Line2D;
 import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
+import java.util.Random;
 
 public class DirectedRandomWalkPlants {
 	private static final int frameWIDTH  = 400; // ImageFrame width
@@ -27,15 +29,21 @@ class ImageFrame extends JFrame {
 	private int imgSize; // height == width
 	private int numStems;
 	private int stepsPerStem;
-	private float transProb; // transmission probablity ϵ [0.0,1.0]
-	private float maxRotInc; // max rotation increment ϵ [0.0,1.0]
-	private int growthInc; // growth segment increment
+	private float transmProb; // transmission probablity ϵ [0.0,1.0]
+	private float maxRotIncPerStep; // max change in rotation per step ϵ [0.0,1.0] radians
+	private int growthIncPerStep; // change in growth segment length per step
 
+	private Random rand; // only one Random obj is needed
+
+	private BufferedImage img = null; // initialize null BufferedImage to change later
+	private Graphics2D g2D;
 
 	//==============================================================
 	// constructor
 
 	public ImageFrame(int width, int height) {
+		this.rand = new Random(); // create new Random object for generating walk sequence
+
 		// setup the frame's attributes
 		this.setTitle("CAP 3027 2016 - HW05 - Brandon Peterson");
 		this.setSize(width, height);
@@ -56,9 +64,20 @@ class ImageFrame extends JFrame {
 					imgSize = promptUserForInt("Enter the desired image size n (for an nxn image).");
 					numStems = promptUserForInt("Enter the desired number of stems.");
 					stepsPerStem = promptUserForInt("Enter the desired number of steps per stem.");
-					transProb = promptUserForFloat("Enter the desired transmission probability.");
-					maxRotInc = promptUserForFloat("Enter the desired maximum rotation increment.");
-					growthInc = promptUserForInt("Enter the desired growth segment increment.");
+					transmProb = promptUserForFloat("Enter the desired transmission probability.");
+					maxRotIncPerStep = promptUserForFloat("Enter the desired maximum rotation increment.");
+					growthIncPerStep = promptUserForInt("Enter the desired growth segment increment.");
+
+					img = new BufferedImage(imgSize, imgSize, BufferedImage.TYPE_INT_ARGB);
+					g2D = (Graphics2D) img.createGraphics();
+					
+					g2D.setColor(Color.WHITE);
+					setBackground(img);
+
+					g2D.setColor(Color.BLACK);
+					DRWP_SingleStem(img);
+
+					displayBufferedImage(img);
 				}
 			}	);
 		fileMenu.add(drwpItem);
@@ -102,7 +121,7 @@ class ImageFrame extends JFrame {
 
 
 	// -----------------------------------------------------------
-	// promptUserForFloatt() -
+	// promptUserForFloat() -
 	// 
 	private float promptUserForFloat(String msg) {
 		String result = JOptionPane.showInputDialog(msg); // get String input from option pane
@@ -121,8 +140,66 @@ class ImageFrame extends JFrame {
 		return val;
 	}
 
+	// ------------------------------------------------------------------
+	// setBackground() - fill entire image with rectangle of specified color
+	// 	params: BufferedImage image = image to change background color of
+	//          int color = desired background color
+	private void setBackground(BufferedImage image) {
+		this.g2D.fillRect(0,0, image.getWidth(), image.getHeight());
+	}
+
+	private void DRWP_SingleStem(BufferedImage image) {
+		double angle = Math.PI/2; // initial angle (stem starts growing "upwards")
+		double rho = 1.0; // initial growth segment length; then becomes 
+		
+		// grow from point (x,y)
+		double x = this.imgSize/2.0; 
+		double y = x;
+
+		// create line obj for initial segment (obj will be resued for additional segments)
+		Line2D.Double stemSegment = new Line2D.Double(x, y, x + rho*Math.cos(angle), y - rho*Math.sin(angle));
+		g2D.draw(stemSegment);
+
+		double reflProb = 1 - this.transmProb; // reflection probability
+		int direction = 1; // +1 = left; -1 = right
+
+		double bias, prevAngle, prevRho;
+		double prevX = x + rho*Math.cos(angle);
+		double prevY = y - rho*Math.sin(angle);
+		while (this.stepsPerStem > 0) {
+			prevAngle = angle;
+			prevRho = rho;
+
+			// determine new bias
+			if (direction == -1) {
+				bias = this.transmProb;
+			}
+			else {
+				bias = reflProb;
+			}
+			// use bias to determine next direction
+			if (rand.nextFloat() > bias) {
+				direction = 1;
+			}
+			else {
+				direction = -1;
+			}
+			// calculate end position of next line to draw
+			rho = prevRho + this.growthIncPerStep;
+			angle = prevAngle + (this.maxRotIncPerStep * rand.nextFloat() * direction);
+
+			stemSegment.setLine(prevX, prevY, prevX + rho*Math.cos(angle), prevY - rho*Math.sin(angle));
+			g2D.draw(stemSegment);
+
+			prevX = prevX + rho*Math.cos(angle);
+			prevY = prevY - rho*Math.sin(angle);
+
+			this.stepsPerStem--;
+		}
+	}
+
 	// ---------------------------------------------------------------
-	// displayBufferedImage() - displayes an image in the content pane
+	// displayBufferedImage() - displays an image in the content pane
 	// 	params: BufferedImage image = image to display
 	public void displayBufferedImage(BufferedImage image) {
 		// display resulting image
